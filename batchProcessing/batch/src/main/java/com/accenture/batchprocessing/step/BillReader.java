21a34a06f571fdb1b52a4aa380dcb1e93018c6da
Poorun <rishikesh.poorun@accenture.com>
2018-05-16 13:35:42 +0400

    rishikesh poorun

--- batchProcessing/batch/src/main/java/com/accenture/batchprocessing/step/BillReader.java
+++ batchProcessing/batch/src/main/java/com/accenture/batchprocessing/step/BillReader.java
@@ -1,6 +1,10 @@
 package com.accenture.batchprocessing.step;
 
-import javax.batch.api.chunk.ItemReader;
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.batch.runtime.BatchStatus;
+import javax.batch.runtime.Metric;
 import javax.batch.runtime.StepExecution;
 
 import org.springframework.batch.core.StepContribution;
@@ -12,6 +16,54 @@ public class BillReader implements Tasklet, StepExecution{
 
 	@Override
 	public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public long getStepExecutionId() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String getStepName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public BatchStatus getBatchStatus() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Date getStartTime() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Date getEndTime() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getExitStatus() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Serializable getPersistentUserData() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Metric[] getMetrics() {
 		// TODO Auto-generated method stub
 		return null;
 	}
